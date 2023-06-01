bbd05a1f6581cb3525d2837a3103bc90a01decee
justin337 <justin@thefitpassport.com>
2023-06-01 11:29:43 -0700

    fix path

--- qlora.py
+++ qlora.py
@@ -465,6 +465,7 @@ def checkDataset(args) :
     path = '/content/qlora/dataset/train-dfhdfghdfghfhwrthwrth.json'
     print(os.path.isfile(path))
     dataset = load_dataset('json', data_files='/content/qlora/dataset/train-dfhdfghdfghfhwrthwrth.json', cache_dir='my_cache', ignore_verifications=True)
+    print(dataset.info)
     print("####### COMPLETE LOADING DATASET ########")
 
     # if args.dataset == 'alpaca':
