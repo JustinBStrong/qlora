38f4490069106da585183a6fa6296d94790f4290
justin337 <justin@thefitpassport.com>
2023-06-19 23:22:21 -0700

    fix script

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ if [ ! -d "$HOME/anaconda3" ]; then
     wget https://repo.anaconda.com/archive/Anaconda3-2023.03-1-Linux-x86_64.sh
     bash Anaconda3-2023.03-1-Linux-x86_64.sh -b -p $HOME/anaconda3
 fi
-
+hash -r conda
 # Add Anaconda to PATH
 export PATH="$HOME/anaconda3/bin:$PATH"
 
